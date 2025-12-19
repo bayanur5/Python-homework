c52b99aaf96a757ff24b840e8a86751664069287
bayanur5 <b.dzhroeva@gmail.com>
2025-12-19 00:16:08 +0000

    added homework-16

---
+++ Homework-16/variables.py
@@ -0,0 +1,17 @@
+my_name = "Bayanur"
+fav_food = "borsh"
+fav_season = "spring"
+
+print (f"Hello my name is {my_name}, and my favorite food is {fav_food}, if your favorite season is {fav_season} let's get along.")
+
+
+name = input("Enter your name: ")
+print (f"""Hello my name is {name}. 
+{name}  is currently studying python. 
+{name} loves DevOps.
+""")
+
+years = int(input("Input number of years: "))
+print (years*365, "days")
+print (years*52, "weeks")
+print (years*365*24, "hours")
